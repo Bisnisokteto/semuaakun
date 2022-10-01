696966af4afa30b1412878830a9fd30acd2da14b
dikazxr <110271297+Bisnisokteto@users.noreply.github.com>
2022-10-01 15:15:54 +0700

    hi
    hi

---
+++ allakun.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay By DikaTools
+echo
+echo $yellow"Selamat datang Nainnadika"
+echo
+echo $white"Mohon tunggu sedang menampilkan data owner [!]"
+sleep 5
+echo $white"Done [✓]"
+sleep 2
+echo
+echo $purple"Nama           : Nainnadika"
+echo $purple"Nama Panggilan : Dika"
+echo $purple"Asal Kota      : Cirebon"
+echo $purple"tanggal lahir  : 21 Mei 2007"
+echo $purple"umur           : 15Th"
+echo
+echo $yellow"Follow Juga sosial media Owner"
+echo
+echo $purple"Github    : Bisnisokteto"
+echo $purple"Whatsapp  : 088223674569"
+echo $purple"Instagram : fixri.xaw.5"
+echo $purple"tiktok    : Dikaa?"
+echo
+echo $white"Mohon Tunggu sedang menampilkan data akun [!]"
+sleep 5
+echo $white"Berhasil [✓]"
+echo
+echo $white"Kamu akan di alihkan ke halaman selanjutnya"
+echo $white"mohon tunggu beberapan saat"
+echo
+echo $white"loading..."
+sleep 5
+echo $white"selesai [✓]"
+sleep 2
+echo $white"Kamu akan di alihkan ke halaman selanjutnya"
+clear
+sleep 3
+figlet DikaTools
+echo $yellow"Selamat datang DikaTools °•°"
+echo $yellow" Dibuat pada : 1 Oktober 2022"
+echo
+echo $white"Tools ini di buat untuk tujuan agar tidak lupa"
+echo
+echo $white"Berikut data akun milik DikaTools"
+echo
+sleep 3
+echo $white"✧──────[Fabebook]───────✧"
+echo $green"> nomer : 088223674569"
+echo $green"> sandi : DIKACUEK21_"
+echo $white"✧──────────···──────────✧"
+echo
+sleep 1
+echo $white"✧──────────···──────────✧"
+echo $green"> nomer : 088223674569"
+echo $green"> sandi : DIKADAVA210507_"
+echo $white"✧──────────···──────────✧"
+echo
+sleep 1
+echo $white"✧──────────···──────────✧"
+echo $green"> nomer : 088223674569"
+echo $green"> sandi : AKUNHEK123_"
+echo $white"✧──────────···──────────✧"
+echo
+echo $yellow"Loading akun email..."
+echo
+sleep 5
+echo $white"✧────────[Email]────────✧"
+echo $green"> email : dikadava742@gmail.com"
+echo $green"> sandi : DIKADAVA21_"
+echo $white"✧──────────···──────────✧"
+echo
+sleep 1
+echo $white"✧──────────···──────────✧"
+echo $green"> email : freefireebnl20@gmail.com"
+echo $green"> sandi : DIKAGANZ_"
+echo $white"✧──────────···──────────✧"
+echo
+sleep 1
+echo $white"✧──────────···──────────✧"
+echo $green"> email : dikaxzyy2@gmail.com"
+echo $green"> sandi : Dikaganteng21_"
+echo $white"✧──────────···──────────✧"
+echo
+sleep 1
+echo $white"✧──────────···──────────✧"
+echo $green"> email : dikaxd33@gmail.com"
+echo $green"> sandi : davacahyamaulana_"
+echo $white"✧──────────···──────────✧"
+echo
+sleep 1
+echo $white"✧──────────···──────────✧"
+echo $green"> email : nainnadika6@gmail.com"
+echo $green"> sandi : davadika_"
+echo $white"✧──────────···──────────✧"
+echo
+sleep 1
+echo $white"✧──────────···──────────✧"
+echo $green"> email : akunhek481@gmail.com"
+echo $green"> sandi : davacahya_"
+echo $white"✧──────────···──────────✧"
+echo
+sleep 1
+echo $white"✧──────────···──────────✧"
+echo $green"> email : bisnisokteto@gmail.com"
+echo $green"> sandi : dikacyz_"
+echo $white"✧──────────···──────────✧"
+echo
+echo
+sleep 1
+echo $cyan"Masi dalam tahap pengembangan"
+echo $cyan"sewaktu-waktu akan berubah"
+echo
+echo $yellow"@ By DikaTools"
+echo $yellow"© 2022"
+exit
+if
